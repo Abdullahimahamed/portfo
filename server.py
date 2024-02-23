6f1b0f89947c1d159705d578b31d587026619b94
Abdullahimahamed <112144504+Abdullahimahamed@users.noreply.github.com>
2024-02-23 09:41:33 -0600

    Add files via upload

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/<username>/<int:post_id>")
+def hello_world(username=None, post_id=None):
+    return render_template('index.html', username=username, post_id=post_id)
+
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/blog")
+def blog():
+    return "<p>These are my thoughts on blogs.</p>"
+
+
+@app.route("/blog/2020/dogs")
+def blog2():
+    return "<p>This is my dog.</p>"
